e8c48983733fba533f4a967c23fdeee271a42a02
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-06-08 17:02:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1
-    array[0]
+  array[0] if array.length == 1
+    # array[0]
   elsif array.length == 2
-    array[0] + " and " + array[1] 
+    # array[0] + " and " + array[1] 
   else
     array[-1] = "and " + array[-1]
     array.join(", ") 
